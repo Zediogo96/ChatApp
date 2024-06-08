84b025fe36a82570c67a6781060b0af37289587e
Zediogo96 <josediogo96@outlook.com>
2024-06-09 00:39:25 +0100

    fix: adjustments to Welcome Page design

--- client/app/stack/Welcome.tsx
+++ client/app/stack/Welcome.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, Image, StyleSheet, Dimensions } from "react-native";
+import { View, Text, Pressable, StyleSheet, Dimensions } from "react-native";
 import React, { useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -8,20 +8,14 @@ import { useNavigation } from "expo-router";
 import Colors from "@/constants/Colors";
 
 import Animated, {
-    BounceIn,
     BounceInLeft,
     Easing,
-    FadeIn,
-    RollInLeft,
     ZoomIn,
     useAnimatedStyle,
     useSharedValue,
     withTiming,
 } from "react-native-reanimated";
-import { SafeAreaView } from "react-native-safe-area-context";
 import TypeAnimation from "@/components/Animated/TypewritterEffect";
-
-import { Circle, ClipPath, Defs, Polyline, Rect, Svg, Image as SvgImage } from "react-native-svg";
 
 // @ts-expect-error: SVG file
 import OnBoardSVG from "@/assets/images/welcome/onBoard.svg";
@@ -48,102 +42,73 @@ const Welcome: React.FC = () => {
 
     return (
         <LinearGradient
-            style={{
-                flex: 1,
-                justifyContent: "flex-start",
-                paddingHorizontal: 20,
-            }}
-            colors={[Colors.mainTheme.warmBeige, Colors.mainTheme.tan]}
+            style={s.container}
+            colors={["transparent", "rgba(255,255,255,0.5)", "rgba(255,255,255,0.7)", "rgba(255,255,255, 1)"]}
+            locations={[0, 0.05, 0.5, 1]}
         >
             <Animated.View style={[{ marginTop: "25%", alignSelf: "center" }, SVGanimatedStyle]}>
                 <OnBoardSVG width={275} height={275} />
             </Animated.View>
 
-            <Animated.Text
-                entering={ZoomIn.easing(Easing.out(Easing.ease)).duration(500).delay(100)}
-                style={{
-                    fontSize: 50,
-                    fontWeight: "800",
-                    color: Colors.mainTheme.offWhite,
-                    marginTop: "10%",
-                }}
-            >
-                Let's Get
-            </Animated.Text>
-            <Animated.Text
-                entering={BounceInLeft.easing(Easing.out(Easing.ease)).duration(500).delay(500)}
-                style={{
-                    fontSize: 46,
-                    fontWeight: "800",
-                    color: Colors.mainTheme.darkOlive,
-                }}
-            >
-                Started
-            </Animated.Text>
+            <View style={{ paddingBottom: "10%" }}>
+                <Animated.Text
+                    entering={ZoomIn.easing(Easing.out(Easing.ease)).duration(500).delay(100)}
+                    style={s.mainText1}
+                >
+                    Let's Get
+                </Animated.Text>
+                <Animated.Text
+                    entering={BounceInLeft.easing(Easing.out(Easing.ease)).duration(500).delay(500)}
+                    style={s.mainText2}
+                >
+                    Started
+                </Animated.Text>
+            </View>
 
             <TypeAnimation
                 sequence={[
                     {
-                        text: "Connect with each other with chatting",
+                        text: "Connect with your friends and family.",
                         typeSpeed: 50,
                     },
                     {
-                        text: "Calling, enjoy Safe and private texting",
+                        text: "Share your moments with the world.",
+                        typeSpeed: 50,
+                    },
+                    {
+                        text: "Join us now, we have puppies!",
                         typeSpeed: 50,
                     },
                 ]}
-                style={{
-                    fontSize: 20,
-                    color: Colors.mainTheme.offWhite,
-                    marginTop: 45,
-                }}
+                style={s.typewritterText}
                 delayBetweenSequence={2000}
                 blinkSpeed={200}
                 loop
             />
 
-            <Button
-                text="Join Now"
-                onPress={() => navigation.navigate("Signup" as never)}
-                buttonContainerStyle={{
-                    backgroundColor: Colors.mainTheme.darkOlive,
-                    marginTop: "30%",
-                    width: "100%",
-                    borderWidth: 1,
-                    borderColor: Colors.mainTheme.oliveGreen,
-                }}
-                textStyle={{
-                    color: Colors.mainTheme.offWhite,
-                }}
-            />
-
-            <View
-                style={{
-                    flexDirection: "row",
-                    marginTop: 12,
-                    justifyContent: "center",
-                }}
-            >
-                <Text
-                    style={{
-                        fontSize: 16,
+            <View style={{ marginBottom: "10%" }}>
+                <Button
+                    text="Join Now"
+                    onPress={() => navigation.navigate("Signup" as never)}
+                    buttonContainerStyle={s.btnContainer}
+                    textStyle={{
                         color: Colors.mainTheme.offWhite,
                     }}
-                >
-                    Already have an account ?
-                </Text>
-                <Pressable onPress={() => navigation.navigate("Login" as never)}>
+                />
+
+                <View style={s.lowerBtnContainer}>
                     <Text
                         style={{
                             fontSize: 16,
-                            color: Colors.mainTheme.oliveGreen,
-                            fontWeight: "bold",
-                            marginLeft: 4,
+                            color: "gray",
                         }}
                     >
-                        Login
+                        Already have an account ?
                     </Text>
-                </Pressable>
+                    <Pressable onPress={() => navigation.navigate("Login" as never)}>
+                        <Text style={s.loginText}>Login</Text>
+                    </Pressable>
+                </View>
             </View>
         </LinearGradient>
     );
@@ -152,37 +117,43 @@ const Welcome: React.FC = () => {
 export default Welcome;
 
 const s = StyleSheet.create({
-    commonSmallerImg: {
-        height: 100,
-        width: 100,
-        borderRadius: 20,
-        position: "absolute",
-    },
+    container: {
+        flex: 1,
 
-    img1: {
-        top: 20,
-        transform: [{ translateX: 20 }, { translateY: 50 }, { rotate: "-15deg" }],
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
     },
-
-    img2: {
-        top: -30,
-        left: 110,
-        transform: [{ translateX: 50 }, { translateY: 50 }, { rotate: "-5deg" }],
+    mainText1: {
+        fontSize: 50,
+        fontWeight: "800",
+        color: Colors.mainTheme.oliveGreen,
+        marginTop: "10%",
     },
-
-    img3: {
-        top: 170,
-        left: -20,
-        transform: [{ translateX: 50 }, { translateY: 50 }, { rotate: "15deg" }],
+    mainText2: {
+        fontSize: 46,
+        fontWeight: "800",
+        color: Colors.mainTheme.darkOlive,
     },
-
-    img4: {
-        height: 200,
-        width: 200,
-        borderRadius: 20,
-        position: "absolute",
-        top: 120,
-        left: 130,
-        transform: [{ translateX: 50 }, { translateY: 50 }, { rotate: "-15deg" }],
+    typewritterText: {
+        fontSize: 20,
+        color: Colors.mainTheme.oliveGreen,
+    },
+    btnContainer: {
+        backgroundColor: Colors.mainTheme.darkOlive,
+        marginTop: "30%",
+        width: "100%",
+        borderWidth: 1,
+        borderColor: Colors.mainTheme.oliveGreen,
+    },
+    lowerBtnContainer: {
+        flexDirection: "row",
+        marginTop: 12,
+        justifyContent: "center",
+    },
+    loginText: {
+        fontSize: 16,
+        color: Colors.mainTheme.oliveGreen,
+        fontWeight: "bold",
+        marginLeft: 4,
     },
 });
