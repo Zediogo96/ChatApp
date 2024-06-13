981f835f4f12ca7d85b75f8c4202247a6cee992d
Zediogo96 <josediogo96@outlook.com>
2024-06-13 21:41:29 +0100

    fix: logout function was not working properly

--- client/store/authStore.ts
+++ client/store/authStore.ts
@@ -5,60 +5,60 @@ import { MMKVStorage, storage } from "@/utils/localStorage";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface State {
-    isAuthenticated: boolean;
-    jwt_token: string | null;
-    user: User | null;
+  isAuthenticated: boolean;
+  jwt_token: string | null;
+  user: User | null;
 }
 
 const initialState: State = {
-    isAuthenticated: false,
-    jwt_token: null,
-    user: null,
+  isAuthenticated: false,
+  jwt_token: null,
+  user: null,
 };
 
 const persistConfig = {
-    name: "my-storage-persist-name",
-    storage: createJSONStorage(() => MMKVStorage),
+  name: "my-storage-persist-name",
+  storage: createJSONStorage(() => MMKVStorage),
 };
 
 type Actions = {
-    setIsAuthenticated: (data: LoginResponse) => void;
-    setUnauthenticated: () => void;
+  setIsAuthenticated: (data: LoginResponse) => void;
+  setUnauthenticated: () => void;
 };
 
 type Store = State & Actions;
 
 // Function currying is a workaround to allow skipping some generics when calling a function with multiple generics
 const useAuthStore = create<Store>()(
-    persist(
-        (set, get) => ({
-            ...initialState,
-            setIsAuthenticated: (data: LoginResponse) => {
-                set({
-                    isAuthenticated: true,
-                    jwt_token: data.token,
-                    user: {
-                        id: data.id,
-                        username: data.username,
-                        email: data.email,
-                        bio: data.bio,
-                        avatar_url: data.avatar_url,
-                        created_at: data.created_at,
-                        updated_at: data.updated_at,
-                    },
-                });
+  persist(
+    (set, get) => ({
+      ...initialState,
+      setIsAuthenticated: (data: LoginResponse) => {
+        set({
+          isAuthenticated: true,
+          jwt_token: data.token,
+          user: {
+            id: data.id,
+            username: data.username,
+            email: data.email,
+            bio: data.bio,
+            avatar_url: data.avatar_url,
+            created_at: data.created_at,
+            updated_at: data.updated_at,
+          },
+        });
 
-                router.replace("/home" as never);
-            },
-            setUnauthenticated: () => {
-                set({ ...initialState });
-                storage.delete("my-storage-persist-name");
-                router.replace("/Login");
-            },
-        }),
+        router.replace("/home" as never);
+      },
+      setUnauthenticated: () => {
+        set({ ...initialState });
+        storage.delete("my-storage-persist-name");
+        router.replace("/stack/Login");
+      },
+    }),
 
-        persistConfig
-    )
+    persistConfig
+  )
 );
 
 export default useAuthStore;
