ca5d9206418fa30aeb80f2d579cd789c84f9d05d
Zediogo96 <josediogo96@outlook.com>
2024-06-13 22:02:24 +0100

    feat: integration with react native gifted chat

--- client/app/(tabs)/home/PrivateConversationPage.tsx
+++ client/app/(tabs)/home/PrivateConversationPage.tsx
@@ -1,32 +1,59 @@
 import { StyleSheet, View } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
 import { useLocalSearchParams } from "expo-router";
 import Colors from "@/constants/Colors";
 import Header from "@/components/PrivateConversationPage/Header";
-import MessagesContainer from "@/components/PrivateConversationPage/MessagesContainer";
+import { useMessagesBySenderID } from "@/api/react-query/messages";
+
+import { GiftedChat } from "react-native-gifted-chat";
 
 type RouterParams = {
-    id: string;
-    name: string;
-    avatar: string;
+  id: string;
+  name: string;
+  avatar: string;
 };
 
 const PrivateConversationPage: React.FC = () => {
-    const { id, name, avatar } = useLocalSearchParams<RouterParams>();
+  const { id, name, avatar } = useLocalSearchParams<RouterParams>();
 
-    return (
-        <View style={[styles.container]}>
-            <Header name={name} avatar={avatar} />
-            <MessagesContainer id={id} />
-        </View>
-    );
+  const { data: messages, isLoading } = useMessagesBySenderID(id);
+
+  const formattedMessages = useMemo(() => {
+    if (!messages) return [];
+    // todo: remove any
+    return messages.map((message: any) => ({
+      _id: message.id,
+      text: message.content,
+      createdAt: new Date(message.created_at),
+      user: {
+        _id: message.sender_id,
+        name: name,
+        avatar: avatar,
+      },
+    }));
+  }, [messages]);
+
+  console.log("formattedMessages %j", JSON.stringify(formattedMessages));
+
+  return (
+    <View style={[styles.container]}>
+      <Header name={name} avatar={avatar} />
+      <GiftedChat
+        messages={formattedMessages}
+        // onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </View>
+  );
 };
 
 export default PrivateConversationPage;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.mainTheme.offWhite,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.mainTheme.offWhite,
+  },
 });
